51e84ab7594d5fb19f18daa12bc2793b81cbdec8
Richard Stiller <stiller@infai.org>
2021-06-13 21:51:06 +0200

    [feat] implement prefill
    * extract scripts and css date in separate files
    * rename vaccination html file with better name

---
+++ src/main/resources/META-INF/resources/dgc/main.js
@@ -0,0 +1,140 @@
+/**
+ * Prefill the form with data based on the vaccine.
+ *
+ * @param {'AstraZeneca'|'Biontech'|'Johnson'|'Moderna'} value
+ */
+function prefillVaccineData(value) {
+    /**
+     * @type {HTMLFormElement}
+     */
+    const form = document.getElementById("vacination-request-form");
+    switch (value) {
+        case "Biontech":
+            form.elements["tg"].value = "840539006" // COVID19
+            form.elements["vp"].value = "1119349007" // mRNA
+            form.elements['mp'].value = "EU/1/20/1528" // Comirnaty
+            form.elements['ma'].value = "ORG-100030215"
+            form.elements['dn'].value = 2
+            form.elements['sd'].value = 2
+            break;
+        case "Johnson":
+            form.elements["tg"].value = "840539006" // COVID19
+            form.elements["vp"].value = "1119305005" // antigen vaccine
+            form.elements['mp'].value = "EU/1/20/1525" // COVID-19 Vaccine Janssen
+            form.elements['ma'].value = "ORG-100001417"
+            form.elements['dn'].value = 1
+            form.elements['sd'].value = 1
+            break;
+        case "Moderna":
+            form.elements["tg"].value = "840539006" // COVID19
+            form.elements["vp"].value = "1119349007" //mRNA
+            form.elements['mp'].value = "EU/1/20/1507" // COVID-19 Vaccine Moderna
+            form.elements['ma'].value = "ORG-100031184"
+            form.elements['dn'].value = 2
+            form.elements['sd'].value = 2
+            break;
+        case "AstraZeneca":
+            form.elements["tg"].value = "840539006" // COVID19
+            form.elements["vp"].value = "1119305005" // antigen vaccine
+            form.elements['mp'].value = "EU/1/21/1529" // Vaxzevria
+            form.elements['ma'].value = "ORG-100001699"
+            form.elements['dn'].value = 2
+            form.elements['sd'].value = 2
+            break;
+    }
+}
+
+/**
+ * @param {HTMLInputElement} field field to set and focus.
+ * @return {void} nothing
+ */
+function setCovid19ToField(field) {
+    field.value = "840539006";
+    field.focus()
+}
+
+async function sendVacinationRequest() {
+    const form = document.getElementById("vacination-request-form");
+
+    if (!form.reportValidity()) {
+        return Promise.reject("invalid data");
+        // TODO display error in frontend
+    }
+
+    const oVacinationRequest = {
+        "nam": {
+            "fn": form.elements["fn"].value,
+            "gn": form.elements["gn"].value
+        },
+        "dob": form.elements["dob"].value,
+        "v": [
+            {
+                "tg": form.elements["tg"].value,
+                "vp": form.elements["vp"].value,
+                "mp": form.elements["mp"].value,
+                "ma": form.elements["ma"].value,
+                "dn": parseInt(form.elements["dn"].value),
+                "sd": parseInt(form.elements["sd"].value),
+                "dt": form.elements["dt"].value
+            }
+        ]
+    };
+    await sendRequest("/api/certify/v2/issue", oVacinationRequest)
+}
+
+function prefillParameters() {
+    const form = document.getElementById("vacination-request-form");
+
+    // remove '#' from hash
+    const params = new URLSearchParams(window.location.hash.substring(1));
+
+    for (const name of ["fn", "gn", "dob", "tg", "vp", "mp", "ma", "dn", "sd", "sd"]) {
+        // setting the values to null will cause the validation to be triggered
+        if (params.has(name)) {
+            form.elements[name].value = params.get(name);
+        }
+    }
+}
+
+async function sendRecoveredRequest() {
+    const form = document.getElementById("request-form");
+    const requestData = {
+        "nam": {
+            "fn": form.elements["fn"].value,
+            "gn": form.elements["gn"].value
+        },
+        "dob": form.elements["dob"].value,
+        "r": [
+            {
+                "id": form.elements["id"].value,
+                "tg": form.elements["tg"].value,
+                "is": form.elements["is"].value,
+                "fr": form.elements["fr"].value,
+                "df": form.elements["df"].value,
+                "du": form.elements["du"].value,
+            }
+        ]
+    };
+    await sendRequest("/api/certify/v2/recovered", requestData);
+}
+
+/**
+ * @param {string} path the request path.
+ * @param {{}} requestData
+ * @return {Promise<void>} open dialog download dialog to download pdf file.
+ */
+async function sendRequest(path, requestData) {
+    try {
+        const response = await fetch(path, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(requestData)
+        });
+        const buffer = await response.arrayBuffer();
+        const blob = new Blob([buffer], {"type": "application/pdf"});
+        window.location = URL.createObjectURL(blob);
+    } catch (e) {
+        console.error(e);
+        // TODO display error code in frontend
+    }
+}
