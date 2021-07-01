c05adef3e6c4667d25ae911e953ba42d630a3339
Tempuser 5432 <temp_user_5432@infai.org>
2021-07-01 16:38:58 +0200

    Add missing export statements

--- launcher/linux/start.sh
+++ launcher/linux/start.sh
@@ -7,29 +7,29 @@
 # are not entered, should be removed from this config file.
 
 # required
-CONNECTOR_BASE_URI=
-CONNECTOR_USER_ID=
+export CONNECTOR_BASE_URI=
+export CONNECTOR_USER_ID=
 
 # required unless specified using X-headers, see doc/dgc-openapi.yml
-CONNECTOR_CLIENT_SYSTEM_ID=
-CONNECTOR_WORKPLACE_ID=
+export CONNECTOR_CLIENT_SYSTEM_ID=
+export CONNECTOR_WORKPLACE_ID=
 
 # optional but recommended; adjust to the connector that is in use, especially CONNECTOR_VERIFY_HOSTNAME=true may
 # cause some problems
-QUARKUS_HTTP_HOST=127.0.0.1
-CONNECTOR_CERT_AUTH_STORE_FILE=
-CONNECTOR_CERT_AUTH_STORE_FILE_PASSWORD=
-CONNECTOR_CERT_TRUST_STORE_FILE=
-CONNECTOR_CERT_TRUST_STORE_FILE_PASSWORD=
-CONNECTOR_USER_PASSWORD=
-CONNECTOR_VERIFY_HOSTNAME=true
+export QUARKUS_HTTP_HOST=127.0.0.1
+export CONNECTOR_CERT_AUTH_STORE_FILE=
+export CONNECTOR_CERT_AUTH_STORE_FILE_PASSWORD=
+export CONNECTOR_CERT_TRUST_STORE_FILE=
+export CONNECTOR_CERT_TRUST_STORE_FILE_PASSWORD=
+export CONNECTOR_USER_PASSWORD=
+export CONNECTOR_VERIFY_HOSTNAME=true
 
 # optional
-CONNECTOR_CARD_HANDLE=
+export CONNECTOR_CARD_HANDLE=
 
 # fixed configuration for PU
-IDP_BASE_URL=https://id.impfnachweis.info/auth/realms/bmg-ti-certify
-IDP_CLIENT_ID=user-access-ti
-DIGITAL_GREEN_CERTIFICATE_SERVICE_ISSUERAPIURL=https://api.impfnachweis.info
+export IDP_BASE_URL=https://id.impfnachweis.info/auth/realms/bmg-ti-certify
+export IDP_CLIENT_ID=user-access-ti
+export DIGITAL_GREEN_CERTIFICATE_SERVICE_ISSUERAPIURL=https://api.impfnachweis.info
 
 java -jar $1
