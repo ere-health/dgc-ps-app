cf714ac7918e81050a02d4d6dc756771260273eb
Tempuser 5432 <temp_user_5432@infai.org>
2021-07-15 13:28:32 +0200

    Add option to specify origin of idp client exception
    Code has been copied from the edited file.

--- src/main/java/health/ere/ps/exception/idp/IdpClientException.java
+++ src/main/java/health/ere/ps/exception/idp/IdpClientException.java
@@ -1,18 +1,46 @@
 package health.ere.ps.exception.idp;
 
 public class IdpClientException extends Exception {
+    public enum Origin {
+        CONNECTOR,
 
-    private static final long serialVersionUID = -3280232274428362763L;
+        IDP,
+
+        INTERNAL,
+    }
+
+    private static final long serialVersionUID = -3280232274428362762L;
+
+    private Origin origin;
+
+    public IdpClientException(final Exception e, final Origin origin) {
+        super(e);
+        this.origin = origin;
+    }
 
     public IdpClientException(final Exception e) {
-        super(e);
+        this(e, Origin.INTERNAL);
+    }
+
+    public IdpClientException(final String s, final Origin origin) {
+        super(s);
+        this.origin = origin;
     }
 
     public IdpClientException(final String s) {
-        super(s);
+        this(s, Origin.INTERNAL);
+    }
+
+    public IdpClientException(final String message, final Exception e, final Origin origin) {
+        super(message, e);
+        this.origin = origin;
     }
 
     public IdpClientException(final String message, final Exception e) {
-        super(message, e);
+        this(message, e, Origin.INTERNAL);
+    }
+
+    public Origin getOrigin() {
+        return origin;
     }
 }
